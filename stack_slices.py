56e46c1fe86a77c38e5570051badd480c747db44
Peter Melchior <peter.m.melchior@gmail.com>
2014-10-21 12:14:44 -0400

    fitsio output for improved memory usage

--- stack_slices.py
+++ stack_slices.py
@@ -9,6 +9,7 @@ from os import system
 from common import *
 from glob import glob
 from math import pi
+import fitsio
 
 def getValues(data, key):
     # what values are used for the slices
@@ -77,15 +78,12 @@ if __name__ == '__main__':
     shape_ra_key = 'ALPHAWIN_J2000_' + band.upper()
     shape_dec_key = 'DELTAWIN_J2000_' + band.upper()
     
-    # keys = [shape_z_key, 'im3shape_' + band.lower() + '_snr', 'im3shape_' + band.lower() + '_radius', 'im3shape_' + band.lower() + '_stamp_size']
-    # splittings = [[0.7, 0.9, 1.1, 1.5], [20,40,60,1000], [0.263,0.789,26.3], [32,48,64,128]]
+    keys = [shape_z_key, 'im3shape_' + band.lower() + '_snr', 'im3shape_' + band.lower() + '_radius', 'im3shape_' + band.lower() + '_stamp_size', 'im3shape_' + band.lower() + '_bulge_flux', 'im3shape_' + band.lower() + '_disc_flux', B_D, 'im3shape_' + band.lower() + '_mask_fraction']
+    splittings = [[0.7, 0.9, 1.1, 1.5], [20,40,60,1000], [0.263,0.789,26.3], [32,48,64,128], 3, 3, 3, [0., 0.2, 0.4, 1]]
     
-    keys = ['im3shape_' + band.lower() + '_info_flag']
-    splittings = [[0,1,8,128,1024, 2**21]]
+    #keys = ['im3shape_' + band.lower() + '_info_flag']
+    #splittings = [[0,1,8,128,1024, 2**21]]
 
-    # keys = ['im3shape_' + band.lower() + '_bulge_flux', 'im3shape_' + band.lower() + '_disc_flux', B_D, 'im3shape_' + band.lower() + '_mask_fraction']
-    # splittings = [3, 3, 3, [0., 0.2, 0.4, 1]]
-    
     outdir = tmpdir + "/" + label
     system('mkdir -p ' + outdir)
 
@@ -95,7 +93,7 @@ if __name__ == '__main__':
         maxrange = 1.1  # deg
 
     matchfile = outdir + '/matches_' + band.lower() + '.bin'
-    stackfile = outdir + "/DeltaSigma.npy"
+    stackfile = outdir + "/DeltaSigma.fits"
 
     if exists(stackfile) is False:
         # open lens catalog, apply selection if desired
@@ -127,121 +125,83 @@ if __name__ == '__main__':
             print "  re-using existing matchfile", matchfile
 
         htmf = HTMFile(matchfile)
-        print "  found ", htmf.n_matches, "matches"
-
-
-        # set up the container for the shears
         Ngal = htmf.n_matches
-        DeltaSigma = np.empty(Ngal, dtype='float32')
-        DeltaSigma_cross = np.empty(Ngal, dtype='float32')
-        radius = np.empty(Ngal, dtype='float32')
-        weight = np.empty(Ngal, dtype='float32')
+        print "  found ", Ngal, "matches"
 
-        # define the slices and set up their index containers
+        # define the slice splittings
         # can be any key in the shape catalog or a function thereof
-        slices = {}
         print "determining slice ranges..."
+        keynames = []
         for i in range(len(keys)):
             if callable(keys[i]):
                 key_name = keys[i].__name__
             else:
                 key_name = keys[i]
-            slices[key_name] = {}
-            # FIXME: there is no selection on the shapes here, e.g. no BG cut
-            values = getValues(shapes, keys[i])
-            # if the slices are set already: find out how many source will fall in each
-            if iterable(splittings[i]):
-                for s in range(len(splittings[i])-1):
-                    Ngal_slice = getSliceMask(values, splittings[i][s], splittings[i][s+1], return_num=True)*(1.*Ngal)/len(shapes)
-                    slices[key_name][s] = np.zeros(Ngal_slice, dtype='int64')
-            # for percentile splits: create equal-size containers and determine limits
-            else:
-                for s in range(splittings[i]):
-                    # CAVEAT: this is not exactly correct, but deviations
-                    # should be minimal for large stacks
-                    # -> Check for the size of the contained before filling it!
-                    slices[key_name][s] = np.zeros(Ngal/splittings[i], dtype='int64')
+            keynames.append(key_name)
+            if hasattr(splittings[i], '__iter__') is False:
                 # remove 2.5% on either side to reduce impact of outliers
                 delta = 95./splittings[i]
                 ranges = [2.5 + k*delta for k in range(splittings[i]+1)]
+                # FIXME: there is no selection on the shapes here, e.g. no BG cut
+                values = getValues(shapes, keys[i])
                 splittings[i] = percentile(values, ranges)
-                del ranges
-            del values
+                del ranges, values
             print "  " + key_name + ":", splittings[i]
+        np.save(outdir + "/splittings.npy", splittings)
+        np.savetxt(outdir + "/keynames.txt", keynames, fmt='%s')
 
-        # remember the last element (of the whole array DeltaSigma) for each
-        # of the slices
-        last_element = {'all': 0}
-        for k,v in slices.iteritems():
-            last_element[k] = {}
-            for vv in v:
-                last_element[k][vv] = 0
-
-        # iterate over all lenses
+        # iterate over all lenses, write DeltaSigma, r, weight into file
         print "stacking lenses..."
+        fits = fitsio.FITS(stackfile, 'rw')
         specz_calib = getSpecZCalibration()
         counter = 0
+        done = 0
         for m1, m2, d12 in htmf.matches():
             lens = lenses[m1]
             shapes_lens = shapes[m2]
+            n_gal = shapes_lens.size
 
             # compute effective Sigma_crit
             z_phot, cz = getSigmaCritCorrection(specz_calib, lens[lens_z_key])
             sigma_crit = getSigmaCritEffective(z_phot, cz, shapes_lens[shape_z_key])
             # determine extent in DeltaSigma array
-            lower, upper = last_element['all'], last_element['all'] + len(m2)
-            elements = np.arange(lower, upper, dtype='int64')
-            DeltaSigma[lower:upper], DeltaSigma_cross[lower:upper] = sigma_crit * tangentialShear(shapes_lens[shape_ra_key], shapes_lens[shape_dec_key], shapes_lens['im3shape_' + band.lower() + '_e1'], -shapes_lens['im3shape_' + band.lower() + '_e2'], lens['RA'], lens['DEC'], computeB=True)
+            data = np.empty(shapes_lens.size, dtype=[('radius', 'f8'), ('DeltaSigma', 'f8'), ('DeltaSigma_x', 'f8'), ('weight', 'f8'), ('slices', 'S%d' % len(keys))])
+            data['DeltaSigma'], data['DeltaSigma_x'] = sigma_crit * tangentialShear(shapes_lens[shape_ra_key], shapes_lens[shape_dec_key], shapes_lens['im3shape_' + band.lower() + '_e1'], -shapes_lens['im3shape_' + band.lower() + '_e2'], lens['RA'], lens['DEC'], computeB=True)
             if coords == "physical":
-                radius[lower:upper] = Ang2Dist(np.array(d12), lens[lens_z_key])
+                data['radius'] = Ang2Dist(np.array(d12), lens[lens_z_key])
             else:
-                radius[lower:upper] = d12
-            weight[lower:upper] = 0.2/(0.2**2 + (0.1*20/shapes_lens['im3shape_' + band.lower() + '_snr'])**2)**0.5/sigma_crit**2
-            last_element['all'] += len(m2)
+                data['radius'] = d12
+            data['weight'] = 0.2/(0.2**2 + (0.1*20/shapes_lens['im3shape_' + band.lower() + '_snr'])**2)**0.5/sigma_crit**2
 
             # get indices for all sources in each slice
             for i in xrange(len(keys)):
-                if callable(keys[i]):
-                    key_name = keys[i].__name__
-                else:
-                    key_name = keys[i]
+                slice_str = np.repeat('-', n_gal)
                 values = getValues(shapes_lens, keys[i])
                 for s in xrange(len(splittings[i])-1):
                     mask = getSliceMask(values, splittings[i][s], splittings[i][s+1])
-                    sum_mask = sum(mask)
-                    # check if slice array can keep all indices
-                    # if not, double its size
-                    if last_element[key_name][s] + sum_mask > slices[key_name][s].size:
-                        slices[key_name][s].resize(max(slices[key_name][s].size * 2,last_element[key_name][s] + sum_mask))
-                        print " (extending slice index list for %s[%d])" % (key_name, s)
-                    slices[key_name][s][last_element[key_name][s] : (last_element[key_name][s] + sum_mask)] = elements[mask]
-                    last_element[key_name][s] += sum_mask
+                    slice_str[mask] = '%d' % s
                     del mask
-                del values
-            del shapes_lens, elements, z_phot, cz, sigma_crit
+                if i == 0:
+                    data['slices'] = slice_str
+                else:
+                    data['slices'] = np.core.defchararray.add(data['slices'], slice_str)
+                del values, slice_str
+            
+            # write out results
+            if counter == 0:
+                fits.write(data)
+            else:
+                fits[-1].append(data)
+            counter += 1
+            done += n_gal
 
             # output status update and do backup
-            save_chunks = lenses.size / 10
             if counter % 100 == 0:
-                print '  lens %d, matched %.2f%%' % (counter, upper*100./htmf.n_matches)
-            if htmf.n_matches > 5e8 and counter > 0 and counter % save_chunks == 0:
-                print '  saving stack backup...'
-                saveStack(outdir, DeltaSigma, DeltaSigma_cross, radius, weight, keys, splittings, slices, last_element)
-            counter += 1
+                print '  lens %d, matched %.2f%%' % (counter, done*100./htmf.n_matches)
+            del data, shapes_lens, z_phot, cz, sigma_crit
 
-        # reduce the size of the containers to last_element
-        DeltaSigma.resize((last_element['all']), refcheck=False)
-        DeltaSigma_cross.resize((last_element['all']), refcheck=False)
-        weight.resize((last_element['all']), refcheck=False)
-        radius.resize((last_element['all']), refcheck=False)
-        for k,v in slices.iteritems():
-            for vv in v.keys():
-                slices[k][vv].resize((last_element[k][vv]), refcheck=False)
+        fits.close()
 
-        # save the entire shebang
-        print "saving stack files"
-        saveStack(outdir, DeltaSigma, DeltaSigma_cross, radius, weight, keys, splittings, slices, last_element, coords)
-        
     else:
         print "stackfile " + stackfile + " already exists."
         print "Delete it or use different label to rerun this script."
