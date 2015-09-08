f6578cd2a1c9cc34241ea734377fc196e526ee4c
Peter Melchior <peter.m.melchior@gmail.com>
2015-09-08 16:38:31 -0400

    removed plotting; updated to new Balrog file with selections

--- balrog_boost/match_balrog2des.py
+++ balrog_boost/match_balrog2des.py
@@ -1,15 +1,8 @@
 #!/bin/env python
 
-import fitsio
-from matplotlib.colors import LogNorm
-from matplotlib.ticker import NullFormatter
-import pylab as plt
-import weighting
+import fitsio, numpy as np, weighting
 from esutil import numpy_util
-
-def modestify(data):
-    galcut = (data['flags'] <= 3) & -( ((data['class_star'] > 0.3) & (data['mag_auto'] < 18.0)) | ((data['spread_model'] + 3*data['spreaderr_model']) < 0.003) | ((data['mag_psf'] > 30.0) & (data['mag_auto'] < 21.0)))
-    return galcut
+import desdb
 
 def getUniqueIndex(data):
     unq, unq_idx, unq_cnt = np.unique(data, return_index=True, return_counts=True)
@@ -20,59 +13,43 @@ def fields_view(arr, fields):
     return np.ndarray(arr.shape, dtype2, arr, 0, arr.strides)
 
 def getResampledIndex(data, weights, size=1):
-    idx = np.empty(size, dtype='i8')
+    """idx = np.empty(size, dtype='i8')
     done = 0
     while done < size:
         draw = np.nonzero(np.random.random(data.shape[0]) < weights/weights.max())[0]
         draw_len = min(size-done, draw.size)
         idx[done:done+draw_len] = draw[:draw_len]
         done += draw_len
+    """
+    idx = np.random.choice(np.arange(data.shape[0], dtype='i8'), size=size, replace=False, p=weights)
     return idx
 
-def plotHistograms(balrog_data, weights, des_data, band):
-    # plot results
-    bins = [np.linspace(20, 25, 51), np.linspace(0.5, 6, 45)]
-
-    fig = plt.figure(figsize=(12, 5))
-    ax = fig.add_subplot(131)
-    ax.hist2d(balrog_data['mag_auto'], balrog_data['flux_radius'], bins=bins, normed=LogNorm, cmap=plt.cm.GnBu_r)
-    ax.set_ylabel('FLUX_RADIUS ' + band)
-    ax.set_xlabel('MAG_AUTO ' + band)
-    ax.text(0.05, 0.95, 'Balrog', color='w', ha='left', va='top', transform=ax.transAxes)
-
-    ax = fig.add_subplot(132)
-    ax.hist2d(balrog_data['mag_auto'], balrog_data['flux_radius'], bins=bins, normed=LogNorm, cmap=plt.cm.GnBu_r, weights=weights)
-    ax.set_xlabel('MAG_AUTO ' + band)
-    ax.yaxis.set_major_formatter(NullFormatter())
-    ax.text(0.05, 0.95, 'Balrog re-weighted', color='w', ha='left', va='top', transform=ax.transAxes)
-
-    ax = fig.add_subplot(133)
-    ax.hist2d(des_data[:,0], des_data[:,1], bins=bins, normed=LogNorm, cmap=plt.cm.GnBu_r)
-    ax.set_xlabel('MAG_AUTO ' + band)
-    ax.yaxis.set_major_formatter(NullFormatter())
-    ax.text(0.05, 0.95, 'DES shapes', color='w', ha='left', va='top', transform=ax.transAxes)
-    plt.subplots_adjust(left=0.06, right=0.97, top=0.95, wspace=0)
-    plt.show()
-
+def saveWeightFile(filename, data, weights):
+    # simply hijack data['mag_psf'] and overwrite with weights
+    newdata = np.empty(data.shape[0], dtype=[('ra', 'f4'), ('dec', 'f4'), ('mag_auto', 'f4'), ('flux_radius', 'f4'), ('match_weight', 'f8')])
+    newdata['ra'] = data['ra']
+    newdata['dec'] = data['dec']
+    newdata['mag_auto'] = data['mag_auto']
+    newdata['flux_radius'] = data['mag_auto']
+    newdata['match_weight'] = weights
+    fits = fitsio.FITS(filename, 'rw')
+    fits.write(newdata)
+    fits.close()
 
 band = 'i'
 
-# get balrog data and eliminate duplicates
-balrog = fitsio.FITS('/n/des/pmelchior/des/SV/catalogs/Balrog/BalrogObsFile-' + band + '.fits')
-w = balrog[1].where('flags == 0')
+balrog = fitsio.FITS('/n/des/suchyta.1/DataFiles/BalrogDB/cluster-lensing-fix-cut/sim-clus-lens-like.fits')
+w = balrog[1].where('modest_i == 0')
 balrog_ids = balrog[1][w]['balrog_index'].astype('i8')
-balrog_unq_idx = getUniqueIndex(balrog_ids)
+balrog_version = balrog[1][w]['version']
+# create almost unique superindex and select unique ones
+balrog_unq_idx = getUniqueIndex(balrog_ids + 100000000 * balrog_version)
 w = w[balrog_unq_idx]
-
-# load data for matching and to make modest cut
-balrog_data = balrog[1]['ra', 'dec', 'balrog_index', 'mag_auto', 'flux_radius', 'flags', 'class_star', 'spread_model', 'spreaderr_model', 'mag_psf'][w]
-modest = modestify(balrog_data)
-balrog_data = balrog_data[modest]
-del balrog_ids, balrog_unq_idx, modest, w
+balrog_data = balrog[1]['alphawin_j2000_' + band, 'deltawin_j2000_' + band, 'mag_auto_' + band, 'flux_radius_' + band][w]
 
 # get that DES shape catalog
 des = fitsio.FITS('/n/des/pmelchior/des/SV/catalogs/v18/des_sv_wl_info.fits')
-wd = des[1].where('sva1_flags == 0 && sva1_gold_flags == 0 && sva1_spte_flags == 0 && sva1_gold_mag_flags == 0 && photoz_bin >= 0 && ngmix_flags == 0 && dec < -40 && 60 < ra && ra< 95')
+wd = des[1].where('sva1_flags == 0 && sva1_gold_flags == 0 && sva1_spte_flags == 0 && sva1_gold_mag_flags == 0 && photoz_bin >= 0 && ngmix_flags == 0 && dec < -40 && 60 < ra && ra < 95')
 des_mag = des[1][wd]['mag_auto_' + band]
 des_ids = des[1][wd]['coadd_objects_id']
 
@@ -86,63 +63,38 @@ del des_mag, des_size, des_ids, des2_ids, idx_
 
 # match 2D distributions according to nearest neighbors
 # separate the 3 photoz-bins
-weights = []
-n_near = 20
-balrog_data_ = np.dstack((balrog_data['mag_auto'], balrog_data['flux_radius']))[0]
+n_near = 10
+balrog_data_ = np.dstack((balrog_data['mag_auto_' + band], balrog_data['flux_radius_' + band]))[0]
+#balrog_data_ = balrog_data['mag_auto_' + band]
 newcat = np.empty(des_data.shape[0], dtype=[('ra', 'f4'), ('dec', 'f4'), ('photoz_bin', 'i1')])
+"""
 done = 0
 for pzb in [0,1,2]:
-    mask = des[1][wd]['photoz_bin'][:] == pzb
+    mask = (des_pzb == pzb)
     wn = weighting.weight_match(balrog_data_, des_data[mask], n_near)
-    weights.append(wn.get_weights())
+    weights = wn.get_weights()
+    
+    # save a balrog catalog with weights
+    #filename = '/n/des/pmelchior/des/SV/catalogs/v18/balrog_matched_ngmix_i_weights_%d.fits' % pzb
+    #saveWeightFile(filename, balrog_data, weights)
+
+    
     # create a matched resmpled balrog catalog with ra,dec,photoz_bin
-    newidx = getResampledIndex(balrog_data, weights[pzb], size=mask.sum())
-    newcat[done:done+newidx.size]['ra'] = balrog_data[newidx]['ra']
-    newcat[done:done+newidx.size]['dec'] = balrog_data[newidx]['dec']
+    newidx = getResampledIndex(balrog_data, weights, size=mask.sum())
+    newcat[done:done+newidx.size]['ra'] = balrog_data[newidx]['alphawin_j2000_' + band]
+    newcat[done:done+newidx.size]['dec'] = balrog_data[newidx]['deltawin_j2000_' + band]
     newcat[done:done+newidx.size]['photoz_bin'] = pzb
     done += newidx.size
+"""
+
+wn = weighting.weight_match(balrog_data_, des_data, n_near)
+weights = wn.get_weights()
+weights /= weights.sum() # not close enough to one
+newidx = getResampledIndex(balrog_data, weights, size=des_data.shape[0])
+newcat['ra'] = balrog_data[newidx]['alphawin_j2000_' + band]
+newcat['dec'] = balrog_data[newidx]['deltawin_j2000_' + band]
+newcat['photoz_bin'] = 0
+    
 newfits = fitsio.FITS('/n/des/pmelchior/des/SV/catalogs/v18/balrog_matched_ngmix_i_photoz-bin.fits', 'rw')
 newfits.write(newcat)
 newfits.close()
-
-
-"""
-# cross-check: i-band weights applied to other bands
-for band in ['g', 'r', 'z']:
-    des_mag = des[1][wd]['mag_auto_' + band]
-    des_size = des2[1]['flux_radius_' + band][:][des2_index]
-
-    balrog2 = fitsio.FITS('/n/des/pmelchior/des/SV/catalogs/Balrog/BalrogObsFile-' + band + '.fits')
-    w2 = balrog2[1].where('flags == 0')
-    balrog2_ids = balrog2[1][w2]['balrog_index'].astype('i8')
-    balrog2_unq_idx = getUniqueIndex(balrog2_ids)
-    w2 = w2[balrog2_unq_idx]
-
-    balrog2_data = balrog2[1]['balrog_index', 'mag_auto', 'flux_radius', 'flags', 'class_star', 'spread_model', 'spreaderr_model', 'mag_psf'][w2]
-    modest = modestify(balrog2_data)
-    balrog2_data = balrog2_data[modest]
-    del balrog2_ids, balrog2_unq_idx, modest, w2
-
-    idx, idx2 = numpy_util.match(balrog_data['balrog_index'], balrog2_data['balrog_index'])
-    
-    fig = plt.figure(figsize=(12, 5))
-    ax = fig.add_subplot(131)
-    ax.hist2d(balrog2_data['mag_auto'][idx2], balrog2_data['flux_radius'][idx2], bins=bins, normed=LogNorm, cmap=plt.cm.GnBu_r)
-    ax.set_ylabel('FLUX_RADIUS ' + band)
-    ax.set_xlabel('MAG_AUTO ' + band)
-    ax.text(0.05, 0.95, 'Balrog', color='w', ha='left', va='top', transform=ax.transAxes)
-                                 
-    ax = fig.add_subplot(132)
-    ax.hist2d(balrog2_data['mag_auto'][idx2], balrog2_data['flux_radius'][idx2], bins=bins, normed=LogNorm, cmap=plt.cm.GnBu_r, weights=weights[idx])
-    ax.set_xlabel('MAG_AUTO ' + band)
-    ax.yaxis.set_major_formatter(NullFormatter())
-    ax.text(0.05, 0.95, 'Balrog re-weighted', color='w', ha='left', va='top', transform=ax.transAxes)
-                                 
-    ax = fig.add_subplot(133)
-    ax.hist2d(des_mag, des_size, bins=bins, normed=LogNorm, cmap=plt.cm.GnBu_r)
-    ax.set_xlabel('MAG_AUTO ' + band)
-    ax.yaxis.set_major_formatter(NullFormatter())
-    ax.text(0.05, 0.95, 'DES shapes', color='w', ha='left', va='top', transform=ax.transAxes)
-    plt.subplots_adjust(left=0.06, right=0.97, top=0.95, wspace=0)
-    plt.show()
-"""
